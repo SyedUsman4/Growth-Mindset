8ad86e2ec6a509f0054672f7335adc0a3d6f7a05
SyedUsman4 <syedu8915@gmail.com>
2025-04-26 13:37:49 +0500

    Growth Mindset

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ st.set_page_config(page_title= "📁 File Converter & Cleaner", layout="wide")
 st.title("📁 File Converter & Cleaner")
 st.write("Upload your CSV or Excel Files to clean the data convert formats effortlessly🚀")
 
-files = st.file_uploader("Upload CSV or Excel Files", type=["csv", "xlsx"], accept_multiple_files=True)
+files =  st.file_uploader("Upload CSV or Excel Files", type=["csv", "xlsx"], accept_multiple_files=True)
 
 if files:
     for file in files:
